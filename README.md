aa557174acc83a2698163853f79c071ec636a379
greg-hammond <45513541+greg-hammond@users.noreply.github.com>
2019-10-13 09:44:16 -0500

    move notes to readme.md

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # websweeper
 classic minesweeper implementation
+
+
+WebSweeper - an Ode to classic 'Minesweeper'
+ 
+ 
+Author:  Greg Hammond  
+ Original development ~2015
+ 
+ Revision History: 
+  - 10/11/2019 rewrote / re-implemented.  Created git repo.
+   
+
+ future work:
+  - have smily face if win
+  - need to get icons for 6/7/8
+
+  - RWD.  @media handling.
+  - Move to Angular/TS
+  - reimplement with canvas?
+  - user select levels / mines / size
+  - 'high scores'
+ 
+ 
--- scripts.js
+++ scripts.js
@@ -1,30 +1,6 @@
 "use strict";
 
 
-/**
- * WebSweeper - an Ode to classic 'Minesweeper'
- * 
- * 
- * Author:  Greg Hammond
- *   Original development ~2015
- * 
- * Revision History: 
- *  - 10/11/2019 rewrote / re-implemented.  Created git repo.
- *   
- *
- * future work:
- *  - have smily face if win
- *  - need to get icons for 6/7/8
- *
- *  - RWD.  @media handling.
- *  - Move to Angular/TS
- *  - reimplement with canvas?
- *  - user select levels / mines / size
- *  - 'high scores'
- * 
- * 
- */
-
 function Game() {
 
     // standard 'expert' is 16x30 grid with 99 mines
